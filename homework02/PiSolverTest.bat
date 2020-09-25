9691d824b9b03c24d0f108ffe0a3c87102263ca7
B.J. Johnson <bjohnson05@gmail.com>
2020-09-25 16:02:41 -0700

    added homework02 evaluation/feedback

---
+++ homework02/PiSolverTest.bat
@@ -0,0 +1,22 @@
+
+javac PiSolver.java
+del testresults.txt
+
+java PiSolver 10                 >  testresults.txt
+java PiSolver 100                >> testresults.txt
+java PiSolver 1000               >> testresults.txt
+java PiSolver 10000              >> testresults.txt
+java PiSolver 100000             >> testresults.txt
+java PiSolver 1000000            >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+java PiSolver 10000000           >> testresults.txt
+
