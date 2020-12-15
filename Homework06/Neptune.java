99bf6cce4b1359af6403c65c9e6968ec621f77ba
Zach Boehnlein <54929673+zachboehnlein@users.noreply.github.com>
2020-12-15 00:26:08 -0800

    neptune file upload

---
+++ Homework06/Neptune.java
@@ -0,0 +1,30 @@
+
+public class Neptune {
+
+
+
+  /**
+   * neptune constructor
+   * @param gofast this would have been the input for the constructor arguments to do the neptune problem
+   * @throws IllegalArgumentException me being sorry
+   *
+   */
+
+   public Neptune( String gofast ) {
+      try{ BrobInt gofaster = new BrobInt(gofast); }
+
+
+      catch( IllegalArgumentException iae ) {
+        System.out.println("integers only plz");
+      }
+}
+   /**
+    * main mathod
+    *  @param args[] array arguments
+    */
+    public static void main( String args[] ) {
+      System.out.println( "Neptune Program temporarily shutdown..." );
+      System.out.println( "Didn't have enough time to finish this up with all my other school stuff, im sowwwy" );
+
+}
+ }
